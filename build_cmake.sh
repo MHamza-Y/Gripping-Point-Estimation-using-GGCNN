d689b367c94eca443ca4d60355cbe1f54c9bf78f
Hamza-Z <m.hamza.yousaf.22@gmail.com>
2021-07-08 20:29:40 +0200

    update cmake sh file

--- build_cmake.sh
+++ build_cmake.sh
@@ -1,6 +1,3 @@
-apt remove cmake
-apt purge --auto-remove cmake
-
 mkdir ~/temp
 cd ~/temp
 wget https://cmake.org/files/v3.21/cmake-3.21.0-rc1.tar.gz
@@ -10,4 +7,3 @@ cd cmake-3.21.0-rc1/
 make -j4
 make install
 cp ./bin/cmake /usr/bin/
-cmake --version
